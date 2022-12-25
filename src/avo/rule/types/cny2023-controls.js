ee799f0febf74a2aa0b49b8966be08eebf7e4c59
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-12-25 16:46:01 +0000

    Add vertical jump

--- src/avo/rule/types/cny2023-controls.js
+++ src/avo/rule/types/cny2023-controls.js
@@ -1,5 +1,5 @@
 import Rule from '@avo/rule'
-import { EXPECTED_TIMESTEP } from '@avo/constants'
+import { EXPECTED_TIMESTEP, LAYERS, TILE_SIZE } from '@avo/constants'
 
 const GRAVITY = 0.7
 const SIDE_SPEED = 0.5
@@ -25,7 +25,33 @@ export default class CNY2023Controls extends Rule {
     this.checkUserInput(timeStep)
   }
 
-  paint (layer = 0) {}
+  paint (layer = 0) {
+    const app = this._app
+    const hero = app.hero
+    const c2d = app.canvas2d
+
+    if (layer === LAYERS.HUD) {
+      // Draw UI data
+      // ----------------
+      const X_OFFSET = TILE_SIZE * 1.5
+      const Y_OFFSET = TILE_SIZE * -1.0
+      const LEFT = X_OFFSET
+      const RIGHT = app.canvasWidth - X_OFFSET
+      const BOTTOM = app.canvasHeight + Y_OFFSET
+      c2d.font = '2em Source Code Pro'
+      c2d.textBaseline = 'bottom'
+      c2d.lineWidth = 8
+
+      let text = `pushY: ${hero?.pushY.toFixed(2)}`
+      c2d.textAlign = 'right'
+      c2d.strokeStyle = '#fff'
+      c2d.strokeText(text, RIGHT, BOTTOM)
+      c2d.fillStyle = '#c44'
+      c2d.fillText(text, RIGHT, BOTTOM)
+      // ----------------
+
+    }
+  }
 
   checkUserInput (timeStep) {
     const app = this._app
@@ -41,6 +67,10 @@ export default class CNY2023Controls extends Rule {
 
     if (keysPressed['ArrowRight']) {
       hero.pushX += SIDE_SPEED / TIME_MODIFIER
+    }
+
+    if (keysPressed['ArrowUp']) {
+        hero.pushY = -20
     }
   }
 }
